0e8f56b23c231128b034d6823bbd980b2013e3c9
vampire <lifanov.a.v@gmail.com>
2014-08-30 21:47:17 +0400

    add image models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,25 @@
+# coding: utf-8
 from django.db import models
 
 # Create your models here.
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=256, verbose_name=u'Название')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = u'Разделы меню'
+        verbose_name = u'Раздел меню'
+
+class ImageItem(models.Model):
+    image = models.ImageField(upload_to=u'upload/', verbose_name=u'Изображение')
+    menuitem = models.ForeignKey(MenuItem, verbose_name=u'Раздел меню')
+
+    def __unicode__(self):
+        return u'{}: {}'.format(self.menuitem.name, self.pk)
+
+    class Meta:
+        verbose_name_plural = u'Изображения'
+        verbose_name = u'Изображение'
