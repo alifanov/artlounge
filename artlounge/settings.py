0b43573f625e1f3ab7da68c8f7c431da4fef3fa9
vampire <lifanov.a.v@gmail.com>
2014-08-30 21:48:05 +0400

    add image models

--- artlounge/settings.py
+++ artlounge/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
+    'south',
+    'app'
 )
 
 MIDDLEWARE_CLASSES = (
