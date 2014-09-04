c95be3c5e422448fe7788b6aadc82d497e30406f
vampire <lifanov.a.v@gmail.com>
2014-09-04 21:07:32 +0400

    add order form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,8 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 from app.models import Order
 class OrderForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+        self.fields['comment'].widget = TextInput()
     class Meta:
         model = Order
