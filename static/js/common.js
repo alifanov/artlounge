16b8a577001fa85c5461d7ab958e3ba19febdbcf
vampire <lifanov.a.v@gmail.com>
2014-09-04 19:40:42 +0400

    add image models

--- static/js/common.js
+++ static/js/common.js
@@ -2,4 +2,22 @@ $(function(){
     $(".gallery-item a").fancybox({
         padding: 0
     });
+
+    $("#bottom-form-id").submit(function(){
+        if($("#id_email").val() == ''){
+            $("#id_email").addClass('error');
+            setTimeout(function(){
+                $("#id_email").removeClass('error')
+            }, 3000);
+            return false;
+        }
+        if($("#id_comment").val() == ''){
+            $("#id_comment").addClass('error');
+            setTimeout(function(){
+                $("#id_comment").removeClass('error')
+            }, 3000);
+            return false;
+        }
+        return false;
+    });
 });
